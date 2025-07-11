4d13a481f4141153b5c5ac28d50d85ae19e34fa7
Pokerdylan <selekta48@gmail.com>
2025-07-12 02:04:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Hii ni demo! Karibu {user.first_name}.")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(8192573503:AAGkm4M2XV922PViP8Gc2cVQEWoP0MVwvMI).build()
+    app = ApplicationBuilder().token("8192573503:AAGkm4M2XV922PViP8Gc2cVQEWoP0MVwvMI").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
